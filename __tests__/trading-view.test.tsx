47a8492fb275ad398e720295da510c96c178b5b6
nadunlanka <nadun_chat@yahoo.com>
2024-03-27 08:16:20 +0530

    Update the test case

--- __tests__/trading-view.test.tsx
+++ __tests__/trading-view.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react'; // Import act
 import TradingView from '../src/components/trading-view';
 import '@testing-library/jest-dom';
 import Container from 'react-bootstrap/Container';
@@ -7,25 +7,31 @@ import React from 'react';
 jest.mock('axios');
 
 describe('This will test trading view component', () => {
-  test('Show loader before fetch the data', () => {
+  test('Show loader before fetch the data', async () => {
     render(
       <Container>
         <TradingView />
       </Container>
     );
-    expect(screen.getAllByText('Loading...')[0]).toBeInTheDocument();
-    expect(screen.getAllByText('Trading View')[0]).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getAllByText('Loading...')[0]).toBeInTheDocument();
+      expect(screen.getAllByText('Trading View')[0]).toBeInTheDocument();
+    });
   });
 
   test('Renders trading view data correctly', async () => {
-    render(<TradingView />);
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    await waitFor(async () => {
+      await act(async () => {
+        render(<TradingView />);
+      });
+    });
 
-    // Wait for the data to be fetched and the loading text to disappear
-    await waitFor(() => expect(screen.queryByText('Loading...')).toBeNull());
-
-    // Assert that the rendered data is correct
-    expect(screen.getAllByText('Coin')[0]).toBeInTheDocument();
-    expect(screen.getAllByText('Price')[0]).toBeInTheDocument();
+    await waitFor(() => {
+      act(() => {
+        expect(screen.queryByText('Loading...')).toBeNull();
+        expect(screen.getAllByText('Coin')[0]).toBeInTheDocument();
+        expect(screen.getAllByText('Price')[0]).toBeInTheDocument();
+      });
+    });
   });
-});
+})
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  setupFiles: ["<rootDir>/__tests__/__data__/global.js"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/__tests__/__data__/"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
 };
